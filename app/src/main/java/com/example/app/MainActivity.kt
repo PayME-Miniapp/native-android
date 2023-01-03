0e17af8c3b9aa6790ca90376365f1499f36868f3
Hiếu BT <hieubt@edoctor.vn>
2023-01-03 16:21:00 +0700

    fix conflict manifest

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.app
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.payme.sdk.PayMEMiniApp
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var openSdkButton: TextView
     companion object {
         @SuppressLint("StaticFieldLeak")
         var payMEMiniApp: PayMEMiniApp? = null
@@ -13,6 +15,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        openSdkButton = findViewById(R.id.open_sdk)
         payMEMiniApp = PayMEMiniApp(this, null, null)
+        openSdkButton.setOnClickListener {
+            payMEMiniApp!!.openMiniApp()
+        }
     }
 }
