99a41a374492f9f8037706c22d32090de396a6b8
Hiếu BT <hieubt@edoctor.vn>
2023-03-16 10:48:33 +0700

    Revert "build: 0.0.39 bank testing"
    This reverts commit 7e12a957b01d88af0b29099acaa8209275ffda6f.

--- sdk/src/main/java/com/payme/sdk/PayMEMiniApp.kt
+++ sdk/src/main/java/com/payme/sdk/PayMEMiniApp.kt
@@ -30,7 +30,7 @@ class PayMEMiniApp(
         internal var onError: ((ActionOpenMiniApp, PayMEError) -> Unit) = { _, _ -> {} }
         internal var onOneSignalSendTags: ((String) -> Unit)? = null
         internal var onOneSignalDeleteTags: ((String) -> Unit)? = null
-        internal val mode : MiniAppMode = MiniAppMode.bank
+        internal val mode : MiniAppMode = MiniAppMode.product
         internal var onChangeEnv: ((String) -> Unit)? = null
     }
 
