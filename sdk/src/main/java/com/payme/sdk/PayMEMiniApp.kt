41634794cf7855970fe164af374a3db20388c629
thinhtruongtien264 <thinhtt@payme.vn>
2023-11-28 13:45:14 +0700

    fix modal

--- sdk/src/main/java/com/payme/sdk/PayMEMiniApp.kt
+++ sdk/src/main/java/com/payme/sdk/PayMEMiniApp.kt
@@ -31,7 +31,8 @@ class PayMEMiniApp(
         // only payme wallet
         internal var onOneSignalSendTags: ((String) -> Unit)? = null
         internal var onOneSignalDeleteTags: ((String) -> Unit)? = null
-        internal var mode: String = "miniapp_product" //miniapp_product, miniapp_sandbox, miniapp_staging, pm_product, pm_staging, pm_sandbox, bank
+        internal var mode: String =
+            "miniapp_product" //miniapp_product, miniapp_sandbox, miniapp_staging, pm_product, pm_staging, pm_sandbox, bank
         internal var onChangeEnv: ((String) -> Unit)? = null
     }
 
@@ -82,24 +83,24 @@ class PayMEMiniApp(
         openMiniAppData: OpenMiniAppDataInterface,
     ) {
         try {
-           if (openType == OpenMiniAppType.modal) {
-               val modal = MiniAppBottomSheetDialog()
-               MiniAppFragment.openType = openType
-               MiniAppFragment.openMiniAppData = openMiniAppData
-               MiniAppFragment.closeMiniApp = {
-                   modal.dismiss()
-               }
-               modal.show((context as FragmentActivity).supportFragmentManager, null)
-               return
-           }
-           if (openType == OpenMiniAppType.screen) {
-            MiniAppFragment.openType = openType
-            MiniAppFragment.openMiniAppData = openMiniAppData
-            val intent = Intent(context, MiniAppActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context.startActivity(intent)
-            return
-           }
+            if (openType == OpenMiniAppType.modal) {
+                val modal = MiniAppBottomSheetDialog()
+                MiniAppFragment.openType = openType
+                MiniAppFragment.openMiniAppData = openMiniAppData
+                MiniAppFragment.closeMiniApp = {
+                    modal.dismiss()
+                }
+                modal.show((context as FragmentActivity).supportFragmentManager, null)
+                return
+            }
+            if (openType == OpenMiniAppType.screen) {
+                MiniAppFragment.openType = openType
+                MiniAppFragment.openMiniAppData = openMiniAppData
+                val intent = Intent(context, MiniAppActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                context.startActivity(intent)
+                return
+            }
         } catch (e: Exception) {
             Log.d(PayMEMiniApp.TAG, "ex cast: ${e.message}")
         }
