e9136d2a7d47be306d086dfa9c8cd19ec29eb38f
thinhtruongtien264 <thinhtt@payme.vn>
2024-02-01 17:13:54 +0700

    import drawable color

--- sdk/src/main/java/com/payme/sdk/ui/MiniAppBottomSheetDialog.kt
+++ sdk/src/main/java/com/payme/sdk/ui/MiniAppBottomSheetDialog.kt
@@ -1,6 +1,8 @@
 package com.payme.sdk.ui
 
 import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
