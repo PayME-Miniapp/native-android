08f7c0d3476bb26369ef5609fd55c14986683efd
thinhtruongtien264 <thinhtt@payme.vn>
2024-04-23 11:48:36 +0700

    fix crash App

--- sdk/src/main/java/com/payme/sdk/camerax/BaseImageAnalyzer.kt
+++ sdk/src/main/java/com/payme/sdk/camerax/BaseImageAnalyzer.kt
@@ -2,10 +2,12 @@ package com.payme.sdk.camerax
 
 import android.annotation.SuppressLint
 import android.graphics.Rect
+import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.android.gms.tasks.Task
 import com.google.mlkit.vision.common.InputImage
+import com.payme.sdk.PayMEMiniApp
 
 abstract class BaseImageAnalyzer<T> : ImageAnalysis.Analyzer {
 
@@ -14,20 +16,25 @@ abstract class BaseImageAnalyzer<T> : ImageAnalysis.Analyzer {
     @SuppressLint("UnsafeExperimentalUsageError", "UnsafeOptInUsageError")
     override fun analyze(imageProxy: ImageProxy) {
         val mediaImage = imageProxy.image
-        mediaImage?.let {
-            detectInImage(InputImage.fromMediaImage(it, imageProxy.imageInfo.rotationDegrees))
-                .addOnSuccessListener { results ->
-                    onSuccess(
-                        results,
-                        graphicOverlay,
-                        it.cropRect
+        try {
+            mediaImage?.let {
+                detectInImage(
+                    InputImage.fromMediaImage(
+                        it,
+                        imageProxy.imageInfo.rotationDegrees
                     )
-                    imageProxy.close()
-                }
-                .addOnFailureListener {
-                    onFailure(it)
-                    imageProxy.close()
-                }
+                ).addOnSuccessListener { results ->
+                        onSuccess(
+                            results, graphicOverlay, it.cropRect
+                        )
+                        imageProxy.close()
+                    }.addOnFailureListener {
+                        onFailure(it)
+                        imageProxy.close()
+                    }
+            }
+        } catch (e: Exception) {
+            Log.d(PayMEMiniApp.TAG, e.toString())
         }
     }
 
@@ -36,9 +43,7 @@ abstract class BaseImageAnalyzer<T> : ImageAnalysis.Analyzer {
     abstract fun stop()
 
     protected abstract fun onSuccess(
-        results: T,
-        graphicOverlay: GraphicOverlay,
-        rect: Rect
+        results: T, graphicOverlay: GraphicOverlay, rect: Rect
     )
 
     protected abstract fun onFailure(e: Exception)
