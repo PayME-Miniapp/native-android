7720f0120e5442ac55930e70c8e5c71d27ffbff4
Trần Minh Đạt <dattm@payme.vn>
2024-07-08 23:05:03 +0700

    build: v0.1.2

--- sdk/src/main/java/com/payme/sdk/network_requests/NetworkRequest.kt
+++ sdk/src/main/java/com/payme/sdk/network_requests/NetworkRequest.kt
@@ -15,93 +15,119 @@ import kotlin.random.Random
 
 object NetworkUtils {
     fun getApiUrl(env: ENV): String {
-        return when(env) {
+        return when (env) {
             ENV.PRODUCTION, ENV.STAGING -> "https://gapi.payme.vn"
             ENV.SANDBOX -> "https://sbx-gapi.payme.vn"
             ENV.DEV, ENV.LOCAL -> "http://vula.mecorp.local:3000"
-            else -> "https://gapi.payme.vn"
         }
     }
 }
 
 internal class NetworkRequest(
     private val context: Context,
-    private val url: String,
-    private val path: String,
+    private val baseUrl: String,
+    private val apiPath: String,
     private val token: String,
-    private val params: MutableMap<String, Any>?,
+    private val params: MutableMap<String, String>,
     private val action: ActionOpenMiniApp
 ) {
     fun setOnRequest(
-        onSuccess: (response: JSONObject) -> Unit,
-        onError: (ActionOpenMiniApp, PayMEError) -> Unit
+        onSuccess: (response: JSONObject) -> Unit, onError: (ActionOpenMiniApp, PayMEError) -> Unit
     ) {
         val cryptoRSA = CryptoRSA()
-        val nextValues = Random.nextInt(0, 10000000)
-        val encryptKey = nextValues.toString()
-        val xAPIKey = cryptoRSA.encrypt(encryptKey)
+        val encryptionKey = Random.nextInt(0, 10000000).toString() // Simplified variable name
+        val encryptedKey = cryptoRSA.encrypt(encryptionKey)
         val cryptoAES = CryptoAES()
-        val xAPIAction = cryptoAES.encryptAES(encryptKey, path)
-        val xAPIMessage =
-            cryptoAES.encryptAES(encryptKey, JSONObject(params as Map<*, *>).toString())
-        val valueParams = xAPIAction + "POST" + token + xAPIMessage + encryptKey
-        val xAPIValidate = cryptoAES.getMD5(valueParams)
-        val body: MutableMap<String, Any> = mutableMapOf()
-        body["x-api-message"] = xAPIMessage
+        val encryptedAction = cryptoAES.encryptAES(encryptionKey, apiPath)
+        val encryptedMessage = cryptoAES.encryptAES(
+            encryptionKey, JSONObject(params as Map<*, *>).toString()
+        )
+
+        // Concatenate values for validation
+        val validationString = encryptedAction + "POST" + token + encryptedMessage + encryptionKey
+        val validationHash = cryptoAES.getMD5(validationString)
+
+        val requestBody: MutableMap<String, Any> = mutableMapOf()
+        requestBody["x-api-message"] = encryptedMessage
+
         val queue = Volley.newRequestQueue(context)
-        val request = object : JsonObjectRequest(
-            Method.POST,
-            url + path,
-            JSONObject(body as Map<*, *>),
+        val request = object : JsonObjectRequest(Method.POST,
+            baseUrl + apiPath,
+            JSONObject(requestBody as Map<*, *>),
             Response.Listener { response ->
                 try {
-                    var finalJSONObject: JSONObject? = null
-                    val jsonObject = JSONObject(response.toString())
-                    val xAPIMessageResponse = jsonObject.getString("x-api-message")
-                    val headers = jsonObject.getJSONObject("headers")
-                    val xAPIKeyResponse = headers.getString("x-api-key")
-                    val decryptKey = cryptoRSA.decrypt(xAPIKeyResponse)
-                    val result = cryptoAES.decryptAES(decryptKey, xAPIMessageResponse)
-                    finalJSONObject = JSONObject(result)
+//                    val finalJSONObject: JSONObject?
+//                    val jsonObject = JSONObject(response.toString())
+//                    val xAPIMessageResponse = jsonObject.getString("x-api-message")
+//                    val headers = jsonObject.getJSONObject("headers")
+//                    val xAPIKeyResponse = headers.getString("x-api-key")
+//                    val decryptKey = cryptoRSA.decrypt(xAPIKeyResponse)
+//                    val result = cryptoAES.decryptAES(decryptKey, xAPIMessageResponse)
+//
+
+                    val headers = response.getJSONObject("headers")
+                    val receivedEncryptedKey = headers.getString("x-api-key")
+                    val decryptedKey = cryptoRSA.decrypt(receivedEncryptedKey)
+                    val decryptedMessage =
+                        cryptoAES.decryptAES(decryptedKey, response.getString("x-api-message"))
+
+                    val finalJSONObject = decryptedMessage?.let { JSONObject(it) }
                     Log.d(PayMEMiniApp.TAG, "PARAMS $params")
                     Log.d(PayMEMiniApp.TAG, "RESPONSE $finalJSONObject")
-                    onSuccess(finalJSONObject)
+                    finalJSONObject?.let { onSuccess(it) } ?: run {
+                        // Handle the case where finalJSONObject is null
+                        onError(
+                            action, PayMEError(
+                                PayMEErrorType.Network,
+                                PayMENetworkErrorCode.CONNECTION_LOST.toString()
+                            )
+                        )
+                    }
                 } catch (error: Exception) {
                     Log.d(PayMEMiniApp.TAG, "error ${error.message}")
                     onError(
-                        action,
-                       PayMEError(PayMEErrorType.Network, PayMENetworkErrorCode.CONNECTION_LOST.toString())
+                        action, PayMEError(
+                            PayMEErrorType.Network, PayMENetworkErrorCode.CONNECTION_LOST.toString()
+                        )
                     )
                 }
             },
             Response.ErrorListener { error ->
-                Log.d(PayMEMiniApp.TAG, "error ${error.networkResponse.statusCode}")
+                val errorCode = when (error) {
+                    is TimeoutError -> PayMENetworkErrorCode.TIMED_OUT.toString()
+                    is NoConnectionError -> PayMENetworkErrorCode.CONNECTION_LOST.toString()
+                    is AuthFailureError -> PayMENetworkErrorCode.OTHER.toString()
+                    is ServerError -> PayMENetworkErrorCode.SERVER_ERROR.toString()
+                    is NetworkError -> PayMENetworkErrorCode.CONNECTION_LOST.toString()
+                    is ParseError -> PayMENetworkErrorCode.DECODE_FAILED.toString()
+                    is VolleyError -> PayMENetworkErrorCode.OTHER.toString()
+                    else -> PayMENetworkErrorCode.OTHER.toString()
+                }
+                Log.d(PayMEMiniApp.TAG, "error $errorCode")
                 onError(
-                    action,
-                    PayMEError(PayMEErrorType.Network, PayMENetworkErrorCode.CONNECTION_LOST.toString())
+                    action, PayMEError(
+                        PayMEErrorType.Network, errorCode
+                    )
                 )
-            }
-        ) {
+            }) {
             override fun getHeaders(): MutableMap<String, String> {
                 val headers: MutableMap<String, String> = mutableMapOf()
                 headers["Authorization"] = token
                 headers["Accept"] = "application/json"
                 headers["Content-Type"] = "application/json"
                 headers["x-api-client"] = PayMEMiniApp.appId
-                headers["x-api-key"] = xAPIKey
-                headers["x-api-action"] = xAPIAction
-                headers["x-api-validate"] = xAPIValidate
+                headers["x-api-key"] = encryptedKey
+                headers["x-api-action"] = encryptedAction
+                headers["x-api-validate"] = validationHash
                 return headers
             }
 
             override fun parseNetworkResponse(response: NetworkResponse?): Response<JSONObject> {
                 return try {
                     val jsonString = String(
-                        response!!.data,
-                        Charset.forName(
+                        response!!.data, Charset.forName(
                             HttpHeaderParser.parseCharset(
-                                response.headers,
-                                PROTOCOL_CHARSET
+                                response.headers, PROTOCOL_CHARSET
                             )
                         )
                     )
@@ -109,16 +135,13 @@ internal class NetworkRequest(
                     jsonResponse.put("headers", JSONObject(response.headers as Map<*, *>))
                     Response.success(jsonResponse, HttpHeaderParser.parseCacheHeaders(response))
                 } catch (e: JSONException) {
-                    Response.error<JSONObject>(ParseError(e))
-                } catch (je: JSONException) {
-                    Response.error<JSONObject>(ParseError(je))
+                    Log.d(PayMEMiniApp.TAG, "JSON parsing error", e)
+                    Response.error(ParseError(e))
                 }
             }
         }
         val defaultRetryPolicy = DefaultRetryPolicy(
-            30000,
-            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+            30000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
         )
         request.retryPolicy = defaultRetryPolicy
         queue.add(request)
--- sdk/src/main/java/com/payme/sdk/presentation/AccountPresentation.kt
+++ sdk/src/main/java/com/payme/sdk/presentation/AccountPresentation.kt
@@ -15,6 +15,57 @@ import com.payme.sdk.network_requests.NetworkUtils
 import org.json.JSONObject
 
 object AccountPresentation {
+    private const val PLATFORM = "ANDROID_V2"
+    private const val CHANNEL = "channel"
+
+    @SuppressLint("HardwareIds")
+    private fun registerDevice(
+        context: Context,
+        action: ActionOpenMiniApp,
+        onError: (ActionOpenMiniApp, PayMEError) -> Unit,
+        onSuccess: (String) -> Unit
+    ) {
+        try {
+            val deviceId =
+                Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+            val params = mutableMapOf(
+                "deviceId" to deviceId,
+                "platform" to PLATFORM,
+                "channel" to CHANNEL,
+                "version" to BuildConfig.SDK_VERSION, // Use VERSION_NAME for app version
+                "buildNumber" to BuildConfig.BUILD_NUMBER.toString() // Use VERSION_CODE for build number
+            )
+
+            NetworkRequest(
+                context,
+                NetworkUtils.getApiUrl(PayMEMiniApp.env),
+                "/be/ewallet/sdk/clientDevice/register",
+                "",
+                params,
+                action
+            ).setOnRequest(onError = onError, onSuccess = { jsonObject ->
+                if (jsonObject.optInt("code") == 204300) {
+                    onSuccess(deviceId)
+                } else {
+                    onError(
+                        action,
+                        PayMEError(
+                            PayMEErrorType.Network,
+                            PayMENetworkErrorCode.CONNECTION_LOST.toString()
+                        )
+                    )
+                }
+            })
+        } catch (e: Exception) {
+            // Log the exception with more context
+            Log.e("AccountRepository", "Error registering device", e)
+            onError(
+                action,
+                PayMEError(PayMEErrorType.Network, PayMENetworkErrorCode.CONNECTION_LOST.toString())
+            )
+        }
+    }
+
     @SuppressLint("HardwareIds")
     fun getAccountInfo(
         context: Context,
@@ -22,64 +73,38 @@ object AccountPresentation {
         onResponse: (ActionOpenMiniApp, JSONObject?) -> Unit,
         onError: (ActionOpenMiniApp, PayMEError) -> Unit
     ) {
-        try {
-            val params: MutableMap<String, Any> = mutableMapOf()
-            val deviceId =
-                Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
-            params["deviceId"] = deviceId
-            params["platform"] = "ANDROID_V2"
-            params["channel"] = "channel"
-            params["version"] = BuildConfig.SDK_VERSION
-            params["buildNumber"] = BuildConfig.BUILD_NUMBER.toString()
-            val requestClient = NetworkRequest(
+        registerDevice(context, ActionOpenMiniApp.GET_ACCOUNT_INFO, onError) { deviceId ->
+            val paramsAccount = mutableMapOf(
+                "phone" to phone,
+                "clientId" to deviceId
+            )
+            NetworkRequest(
                 context,
                 NetworkUtils.getApiUrl(PayMEMiniApp.env),
-                "/be/ewallet/sdk/clientDevice/register",
+                "/be/ewallet/sdk/account/getAccountInfo",
                 "",
-                params,
+                paramsAccount,
                 ActionOpenMiniApp.GET_ACCOUNT_INFO
-            )
-            requestClient.setOnRequest(onError = onError, onSuccess = { jsonObject ->
-                val code = jsonObject.optInt("code")
-                if (code == 204300) {
-                    val paramsAccount: MutableMap<String, Any> = mutableMapOf()
-                    paramsAccount["phone"] = phone
-                    paramsAccount["clientId"] = deviceId
-                    val request = NetworkRequest(
-                        context,
-                        NetworkUtils.getApiUrl(PayMEMiniApp.env),
-                        "/be/ewallet/sdk/account/getAccountInfo",
-                        "",
-                        paramsAccount,
-                        ActionOpenMiniApp.GET_ACCOUNT_INFO
-                    )
-                    request.setOnRequest(onError = onError, onSuccess = { jsonObjectAccount ->
-                        val data = jsonObjectAccount.optJSONObject("data")
-                        val accountInfoObject = data?.optJSONObject("accountInfo")
-                        val accountId = accountInfoObject?.optInt("accountId", -1)
-                        if (accountId != -1) {
-                            onResponse(ActionOpenMiniApp.GET_ACCOUNT_INFO, accountInfoObject)
-                        } else {
-                            onError(
-                                ActionOpenMiniApp.GET_ACCOUNT_INFO,
-                                PayMEError(PayMEErrorType.MiniApp, PayMENetworkErrorCode.OTHER.toString(), "Không lấy được thông tin tài khoản")
-                            )
-                        }
-                    })
+            ).setOnRequest(onError = onError, onSuccess = { jsonObjectAccount ->
+                val accountInfoObject =
+                    jsonObjectAccount.optJSONObject("data")?.optJSONObject("accountInfo")
+                val accountId = accountInfoObject?.optInt("accountId", -1) ?: -1
+                if (accountId != -1) {
+                    onResponse(ActionOpenMiniApp.GET_ACCOUNT_INFO, accountInfoObject)
                 } else {
                     onError(
                         ActionOpenMiniApp.GET_ACCOUNT_INFO,
-                        PayMEError(PayMEErrorType.Network, PayMENetworkErrorCode.CONNECTION_LOST.toString())
+                        PayMEError(
+                            PayMEErrorType.MiniApp,
+                            PayMENetworkErrorCode.OTHER.toString(),
+                            "Không lấy được thông tin tài khoản"
+                        )
                     )
                 }
             })
-        } catch (e: Exception) {
-            onError(
-                ActionOpenMiniApp.GET_ACCOUNT_INFO,
-                PayMEError(PayMEErrorType.Network, PayMENetworkErrorCode.CONNECTION_LOST.toString())
-            )
         }
     }
+
     @SuppressLint("HardwareIds")
     fun getBalance(
         context: Context,
@@ -87,87 +112,50 @@ object AccountPresentation {
         onResponse: (ActionOpenMiniApp, JSONObject?) -> Unit,
         onError: (ActionOpenMiniApp, PayMEError) -> Unit
     ) {
-        try {
-            val params: MutableMap<String, Any> = mutableMapOf()
-            params["phone"] = phone
-            val deviceId =
-                Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
-            params["deviceId"] = deviceId
-            params["platform"] = "ANDROID_V2"
-            params["channel"] = "channel"
-            params["version"] = BuildConfig.SDK_VERSION
-            params["buildNumber"] = BuildConfig.BUILD_NUMBER.toString()
-            val requestClient = NetworkRequest(
+        registerDevice(context, ActionOpenMiniApp.GET_BALANCE, onError) { deviceId ->
+            val paramsAccount = mutableMapOf(
+                "phone" to phone,
+                "clientId" to deviceId
+            )
+            NetworkRequest(
                 context,
                 NetworkUtils.getApiUrl(PayMEMiniApp.env),
-                "/be/ewallet/sdk/clientDevice/register",
+                "/be/ewallet/sdk/account/getAccountInfo",
                 "",
-                params,
+                paramsAccount,
                 ActionOpenMiniApp.GET_BALANCE
-            )
-            requestClient.setOnRequest(onError = onError, onSuccess = { jsonObject ->
-                val code = jsonObject.optInt("code")
-                if (code == 204300) {
-                    val paramsAccount: MutableMap<String, Any> = mutableMapOf()
-                    paramsAccount["phone"] = phone
-                    paramsAccount["clientId"] = deviceId
-                    val request = NetworkRequest(
+            ).setOnRequest(onError = onError, onSuccess = { jsonObjectAccount ->
+                val data = jsonObjectAccount.optJSONObject("data")
+                val accessToken = data?.optString("accessToken") // Simplify accessToken extraction
+
+                val responseJson = JSONObject()
+                if (accessToken.isNullOrEmpty()) {
+                    responseJson.put("linked", false)
+                    responseJson.put("message", "Tài khoản chưa được liên kết")
+                    onResponse(ActionOpenMiniApp.GET_BALANCE, responseJson)
+                } else {
+                    val paramsBalance = mutableMapOf("clientId" to deviceId)
+                    NetworkRequest(
                         context,
                         NetworkUtils.getApiUrl(PayMEMiniApp.env),
-                        "/be/ewallet/sdk/account/getAccountInfo",
-                        "",
-                        paramsAccount,
+                        "/be/ewallet/sdk/account/balance",
+                        accessToken,
+                        paramsBalance,
                         ActionOpenMiniApp.GET_BALANCE
-                    )
-                    request.setOnRequest(onError = onError, onSuccess = { jsonObjectAccount ->
-                        val data = jsonObjectAccount.optJSONObject("data")
-                        val accessToken = if (data?.isNull("accessToken") == true) null else data?.optString("accessToken", "")
-                        if (accessToken.isNullOrEmpty()) {
-                            val json = JSONObject()
-                            json.put("linked", false)
-                            json.put("message", "Tài khoản chưa được liên kết")
-                            onResponse(ActionOpenMiniApp.GET_BALANCE, json)
-                        } else {
-                            val paramsBalance: MutableMap<String, Any> = mutableMapOf()
-                            val requestBalance = NetworkRequest(
-                                context,
-                                NetworkUtils.getApiUrl(PayMEMiniApp.env),
-                                "/be/ewallet/sdk/account/balance",
-                                accessToken,
-                                paramsBalance,
-                                ActionOpenMiniApp.GET_BALANCE
-                            )
-                            requestBalance.setOnRequest(onError = onError, onSuccess = {
-                                val dataBalance = it.optJSONObject("data")
-                                val balance = dataBalance?.opt("balance") as Number?
-                                val jsonObjectResponse = JSONObject()
-                                Log.d("PAYMELOG", "balance $balance")
-                                if (balance == null) {
-                                    jsonObjectResponse.put("linked", true)
-                                    jsonObjectResponse.put("balance", 0)
-                                    jsonObjectResponse.put("message", it.optString("message") ?: "Có lỗi xảy ra")
-                                    onResponse(ActionOpenMiniApp.GET_BALANCE, jsonObjectResponse)
-                                } else {
-                                    jsonObjectResponse.put("linked", true)
-                                    jsonObjectResponse.put("balance", balance)
-                                    jsonObjectResponse.put("message", it.optString("message") ?: "Có lỗi xảy ra")
-                                    onResponse(ActionOpenMiniApp.GET_BALANCE, jsonObjectResponse)
-                                }
-                            })
-                        }
+                    ).setOnRequest(onError = onError, onSuccess = { balanceResponse ->
+                        val dataBalance = balanceResponse.optJSONObject("data")
+                        val balance = dataBalance?.opt("balance") as? Number
+                            ?: 0 // Use elvis operator for default balance
+                        responseJson.put("linked", true)
+                        responseJson.put("balance", balance)
+                        responseJson.put(
+                            "message",
+                            balanceResponse.optString("message") ?: "Có lỗi xảy ra"
+                        )
+                        onResponse(ActionOpenMiniApp.GET_BALANCE, responseJson)
                     })
-                } else {
-                    onError(
-                        ActionOpenMiniApp.GET_BALANCE,
-                        PayMEError(PayMEErrorType.Network, PayMENetworkErrorCode.CONNECTION_LOST.toString())
-                    )
                 }
             })
-        } catch (e: Exception) {
-            onError(
-                ActionOpenMiniApp.GET_BALANCE,
-                PayMEError(PayMEErrorType.Network, PayMENetworkErrorCode.CONNECTION_LOST.toString())
-            )
         }
     }
 }
