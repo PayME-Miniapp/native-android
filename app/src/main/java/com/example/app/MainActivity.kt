898bcc9411df41aa6d8cec501512574ceec94c49
NguyetLuong99 <nguyetltt@payme.vn>
2025-04-08 16:24:07 +0700

    feat: EWALLET-1468 Thêm flow scan QR trên Thẻ căn cước

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -12,6 +12,7 @@ import com.payme.sdk.PayMEMiniApp
 import com.payme.sdk.models.ActionOpenMiniApp
 import com.payme.sdk.models.ENV
 import com.payme.sdk.models.Locale
+import com.payme.sdk.models.OpenMiniAppOpenData
 import com.payme.sdk.models.OpenMiniAppTransferQRData
 import com.payme.sdk.models.OpenMiniAppType
 import com.payme.sdk.models.PayMEError
@@ -149,35 +150,35 @@ class MainActivity : AppCompatActivity() {
 //            )
 
 
-            payMEMiniApp!!.openMiniApp(
-                OpenMiniAppType.modal, OpenMiniAppTransferQRData(
-                    "0567777801",
-                    TransferQRData(
-                        amount = 2000000,
-                        bankNumber = "9704000000000018",
-                        swiftCode = "SBITVNVX",
-                        cardHolder = "NGUYEN VAN A",
-                        note = "Test",
-                        partnerTransaction = "123456",
-                        extraData = mapOf(
-                            "key1" to "value1",
-                            "key2" to 123,
-                            "key3" to mapOf(
-                                "street" to "123 Main St",
-                                "city" to "New York",
-                                "country" to "USA"
-                            )
-                        ),
-                        isShowResult = true
-                    )
-                )
-            )
+//            payMEMiniApp!!.openMiniApp(
+//                OpenMiniAppType.modal, OpenMiniAppTransferQRData(
+//                    "0567777801",
+//                    TransferQRData(
+//                        amount = 2000000,
+//                        bankNumber = "9704000000000018",
+//                        swiftCode = "SBITVNVX",
+//                        cardHolder = "NGUYEN VAN A",
+//                        note = "Test",
+//                        partnerTransaction = "123456",
+//                        extraData = mapOf(
+//                            "key1" to "value1",
+//                            "key2" to 123,
+//                            "key3" to mapOf(
+//                                "street" to "123 Main St",
+//                                "city" to "New York",
+//                                "country" to "USA"
+//                            )
+//                        ),
+//                        isShowResult = true
+//                    )
+//                )
+//            )
 //            payMEMiniApp!!.openMiniApp(
 //                OpenMiniAppType.modal,  OpenMiniAppPaymentData("0795550301",
 //                    PaymentData("348115135612", 10000, "", "", true)
 //                )
 //            )
-//             payMEMiniApp!!.openMiniApp(OpenMiniAppType.modal, OpenMiniAppOpenData("0795550301"))
+             payMEMiniApp!!.openMiniApp(OpenMiniAppType.screen, OpenMiniAppOpenData("0328001141"))
 //                        payMEMiniApp!!.openMiniApp(OpenMiniAppType.modal, OpenMiniAppPayME("0795550300", ServiceData("POWE", isBackToApp = true, isShowResult = true)))
         }
     }
--- sdk/src/main/java/com/payme/sdk/ui/MiniAppFragment.kt
+++ sdk/src/main/java/com/payme/sdk/ui/MiniAppFragment.kt
@@ -77,7 +77,6 @@ import vn.kalapa.ekyc.KalapaSDKResultCode
 import vn.kalapa.ekyc.KalapaScanNFCCallback
 import vn.kalapa.ekyc.KalapaScanNFCError
 import vn.kalapa.ekyc.models.KalapaResult
-import vn.kalapa.ekyc.models.PreferencesConfig
 import java.io.File
 import java.net.URL
 import javax.net.ssl.SSLException
@@ -139,7 +138,6 @@ class MiniAppFragment : Fragment() {
     private lateinit var lottieContainerView: LinearLayout
     private lateinit var loadingView: View
 
-    private var preferencesConfig: PreferencesConfig? = null
     private var fileChooserCallback: ValueCallback<Array<Uri>>? = null
     private var faceAuthenData: JSONObject? = null
 
@@ -1229,10 +1227,12 @@ class MiniAppFragment : Fragment() {
     private fun startEKYC(data: JSONObject) {
         val sessionId = data.optString("token", "")
         if (sessionId != "") {
-            val sdkConfig = KalapaSDKConfig.KalapaSDKConfigBuilder(requireContext())
+            val sdkConfig = KalapaSDKConfig.KalapaSDKConfigBuilder(requireContext() as Activity)
                 .withBackgroundColor("#FFFFFF").withMainColor("#33CB33").withBtnTextColor("#121212")
                 .withMainTextColor("#121212").withLivenessVersion(0).withNFCTimeoutInSeconds(180)
-                .withLanguage(PayMEMiniApp.locale.toString()).build()
+                .withLanguage(PayMEMiniApp.locale.toString())
+                .requireQRCode(true)
+                .build()
             val klpHandler = object : KalapaHandler() {
 
                 override fun onComplete(kalapaResult: KalapaResult) {
@@ -1389,10 +1389,12 @@ class MiniAppFragment : Fragment() {
     private fun startNFC(data: JSONObject) {
         val sessionId = data.optString("token", "")
         if (sessionId != "") {
-            val sdkConfig = KalapaSDKConfig.KalapaSDKConfigBuilder(requireContext())
+            val sdkConfig = KalapaSDKConfig.KalapaSDKConfigBuilder(requireContext() as Activity)
                 .withBackgroundColor("#FFFFFF").withMainColor("#33CB33").withBtnTextColor("#121212")
                 .withMainTextColor("#121212").withLivenessVersion(0).withNFCTimeoutInSeconds(180)
-                .withLanguage(PayMEMiniApp.locale.toString()).build()
+                .withLanguage(PayMEMiniApp.locale.toString())
+                .requireQRCode(true)
+                .build()
             val klpHandler = object : KalapaHandler() {
 
                 override fun onComplete(kalapaResult: KalapaResult) {
